2b2687b922c455db41299f6867d9f08753eab0a6
Florian Wechsung <wechsung@maths.ox.ac.uk>
2016-11-09 11:28:51 +0000

    readme

--- stokes_preconditioner_comparison.py
+++ stokes_preconditioner_comparison.py
@@ -1,11 +1,3 @@
-"""
-Comparison of two preconditioners for Stokes equations.
-    + Inverse Mass Diagonal
-    + BFBT Preconditioner. Notes:
-        + Using the BFBT preconditioner for Dirichlet boundary conditions is tricky. Some authors suggest different types of scaling around the boundaries. We notes that natural boundary conditions seem to result in mesh independence in the simple 2d case. this needs to be investigated further.
-        + BFBT type preconditioner only work on quadrilateral meshes!
-"""
-
 from firedrake import *
 from firedrake.petsc import PETSc
 import numpy as np
